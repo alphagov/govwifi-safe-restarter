c82558629d84bc42e86d96ac871d2246633d8801
Emile <emile.swarts123@gmail.com>
2018-11-25 12:22:09 +0000

    Introduce a health checker and stop tasks if it is healthy

--- lib/use_case/safe_restart.rb
+++ lib/use_case/safe_restart.rb
@@ -1,14 +1,25 @@
 module UseCase
   class SafeRestart
-    def initialize(ecs_gateway:)
+    def initialize(ecs_gateway:, health_checker:)
       @ecs_gateway = ecs_gateway
+      @health_checker = health_checker
     end
 
     def safe_restart
       cluster_arns = ecs_gateway.list_clusters
 
-      cluster_arns.each do |arn|
-        tasks = ecs_gateway.list_tasks(cluster: arn)
+      cluster_arns.each do |cluster_arn|
+        task_arns = ecs_gateway.list_tasks(cluster: cluster_arn)
+
+        task_arns.each do |task_arn|
+          if health_checker.healthy?(task_arn)
+            ecs_gateway.stop_task(
+              cluster: cluster_arn,
+              task: task_arn,
+              reason: 'AUTOMATED RESTART'
+            )
+          end
+        end
       end
 
       { status: :success, errors: [] }
@@ -16,6 +27,6 @@ module UseCase
 
     private
 
-    attr_reader :ecs_gateway
+    attr_reader :ecs_gateway, :health_checker
   end
 end
--- spec/use_case/safe_restart_spec.rb
+++ spec/use_case/safe_restart_spec.rb
@@ -2,7 +2,14 @@ require_relative '../../lib/use_case/safe_restart'
 
 describe UseCase::SafeRestart do
   let(:ecs_gateway) { double(list_clusters: []) }
-  subject { described_class.new(ecs_gateway: ecs_gateway) }
+  let(:health_checker) { double(healthy?: false) }
+
+  subject do
+    described_class.new(
+      ecs_gateway: ecs_gateway,
+      health_checker: health_checker
+    )
+  end
 
   describe 'Safe Restart success' do
     it 'returns success' do
@@ -17,19 +24,39 @@ describe UseCase::SafeRestart do
 
       context 'Tasks' do
         let(:some_cluster_arn) { 'arn:aws:ecs:eu-west-2:123:cluster/some-cluster' }
-        let(:ecs_gateway) { double(list_clusters: [some_cluster_arn]) }
+        let(:some_task_arn) { 'arn:aws:ecs:eu-west-2:123:task/fe2c820a-98d9' }
+        let(:ecs_gateway) do
+          double(
+            list_clusters: [some_cluster_arn],
+            list_tasks: [some_task_arn],
+            stop_task: true,
+          )
+        end
 
         before do
-          allow(ecs_gateway).to receive(:list_tasks)
+          subject.safe_restart
         end
 
         it 'calls list_tasks on the ECS gateway' do
-          subject.safe_restart
           expect(ecs_gateway).to have_received(:list_tasks).with(cluster: some_cluster_arn)
         end
 
         context 'Health Check' do
+          context 'Healthy' do
+            let(:health_checker) { double(healthy?: true) }
 
+            before do
+              allow(ecs_gateway).to receive(:stop_task)
+            end
+
+            it 'calls list_tasks on the ECS gateway' do
+              expect(ecs_gateway).to have_received(:stop_task).with(
+                cluster: some_cluster_arn,
+                task: some_task_arn,
+                reason: 'AUTOMATED RESTART'
+              )
+            end
+          end
         end
       end
     end
