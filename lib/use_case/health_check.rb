a8603ed4181790447468e18e11fe327fa146559f
Emile <emile.swarts123@gmail.com>
2018-12-10 12:02:48 +0000

    Use Route53 for health checks

--- lib/use_case/health_check.rb
+++ lib/use_case/health_check.rb
@@ -1,20 +1,33 @@
 module UseCase
   class HealthCheck
-    def initialize(cloudwatch_logs_gateway:)
-      @cloudwatch_logs_gateway = cloudwatch_logs_gateway
+    def initialize(route53_gateway: Gateways::Route53.new)
+      @route53_gateway = route53_gateway
     end
 
-    def execute(log_group_name:)
-      cloudwatch_logs_gateway.healthy?(
-        log_group_name: log_group_name,
-        filter_pattern: HEALTHY_MATCH_PATTERN
-      )
+    def execute
+      health_checks.map do |health_check|
+        status(route53_gateway.get_health_check_status(health_check_id: health_check.id))
+      end.all?
     end
 
     private
 
-    HEALTHY_MATCH_PATTERN = 'Access-Accept'.freeze
+    attr_reader :route53_gateway
 
-    attr_reader :cloudwatch_logs_gateway
+    SUCCESS_STATUS = 'Success: HTTP Status Code 200, OK'.freeze
+
+    def status(health_check)
+      all_health_checkers_healthy?(health_check)
+    end
+
+    def all_health_checkers_healthy?(health_check)
+      health_check.health_check_observations.all? do |a|
+        a.status_report.status == SUCCESS_STATUS
+      end
+    end
+
+    def health_checks
+      @health_checks ||= route53_gateway.list_health_checks.health_checks
+    end
   end
 end
--- spec/use_case/health_check_spec.rb
+++ spec/use_case/health_check_spec.rb
@@ -1,20 +1,126 @@
-# describe UseCase::HealthCheck do
-#   subject do
-#     described_class.new(
-#       cloudwatch_logs_gateway: cloudwatch_logs_gateway
-#     )
-#   end
+class FakeHealthyRoute53Gateway
+  def get_health_check_status(health_check_id:)
+    client = Aws::Route53::Client.new(stub_responses: true)
 
-#   let(:cloudwatch_logs_gateway) { double(healthy?: nil) }
+    client.stub_responses(:get_health_check_status,
+      health_check_observations:
+        [
+          {
+            region: 'ap-southeast-2',
+            ip_address: '39.239.222.111',
+            status_report: {
+              status: 'Success: HTTP Status Code 200, OK'
+            }
+          }, {
+            region: 'ap-eu-west-1',
+            ip_address: '27.111.39.33',
+            status_report: {
+              status: 'Success: HTTP Status Code 200, OK'
+            }
+          }
+        ])
 
-#   context 'Given the Cloudwatch Logs Gateway' do
-#     it 'calls healthy? on it' do
-#       subject.execute(log_group_name: 'SomeLogGroupName')
-#       expect(cloudwatch_logs_gateway).to have_received(:healthy?)
-#         .with(
-#           log_group_name: 'SomeLogGroupName',
-#           filter_pattern: 'Access-Accept'
-#         )
-#     end
-#   end
-# end
+    client.get_health_check_status(health_check_id: health_check_id)
+  end
+
+  def list_health_checks
+    client = Aws::Route53::Client.new(
+      stub_responses: {
+        list_health_checks: {
+          max_items: 10,
+          marker: 'PageMarker',
+          is_truncated: false,
+          health_checks: [
+            {
+              caller_reference: 'AdminMonitoring',
+              id: 'abc123',
+              health_check_version: 1,
+              health_check_config: {
+                measure_latency: false,
+                type: 'HTTP',
+              }
+            }, {
+              caller_reference: 'AdminMonitoring',
+              id: 'xyz789',
+              health_check_version: 1,
+              health_check_config: {
+                measure_latency: false,
+                type: 'HTTP',
+              }
+            }
+          ]
+        }
+      }
+    )
+
+    client.list_health_checks
+  end
+end
+
+class FakeUnHealthyRoute53Gateway
+  def get_health_check_status(health_check_id:)
+    client = Aws::Route53::Client.new(stub_responses: true)
+
+    client.stub_responses(:get_health_check_status,
+      health_check_observations:
+        [
+          {
+            region: 'ap-southeast-2',
+            ip_address: '39.239.222.111',
+            status_report: {
+              status: 'Failure: HTTP Status Code 500, Host Unreachable'
+            }
+          }
+        ])
+
+    client.get_health_check_status(health_check_id: health_check_id)
+  end
+
+  def list_health_checks
+    client = Aws::Route53::Client.new(
+      stub_responses: {
+        list_health_checks: {
+          max_items: 10,
+          marker: 'PageMarker',
+          is_truncated: false,
+          health_checks: [
+            {
+              caller_reference: 'AdminMonitoring',
+              id: 'latency123',
+              health_check_version: 1,
+              health_check_config: {
+                ip_address: '123.123.123.123',
+                measure_latency: false,
+                type: 'HTTP',
+              }
+            }
+          ]
+        }
+      }
+    )
+
+    client.list_health_checks
+  end
+end
+
+describe UseCase::HealthCheck do
+  let(:aws_route53_gateway) { FakeHealthyRoute53Gateway.new }
+
+  let(:result) do
+    described_class.new(route53_gateway: aws_route53_gateway).execute
+  end
+
+  context 'Given health checkers are healthy' do
+    it 'returns operational if all health checkers are healthy' do
+      expect(result).to eq(true)
+    end
+  end
+
+  context 'Given some checkers are unhealthy' do
+    let(:aws_route53_gateway) { FakeUnHealthyRoute53Gateway.new }
+
+    it 'returns an offline status' do
+      expect(result).to eq(false)
+    end
+  end
+end
