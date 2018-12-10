38e60809a4aa49dea72040c37b45a367d3bb8a19
Emile <emile.swarts123@gmail.com>
2018-12-10 11:27:52 +0000

    Test that the Health Checker calls the Gateway correctly

--- lib/use_case/health_check.rb
+++ lib/use_case/health_check.rb
@@ -1,5 +1,20 @@
 module UseCase
   class HealthCheck
+    def initialize(cloudwatch_logs_gateway:)
+      @cloudwatch_logs_gateway = cloudwatch_logs_gateway
+    end
 
+    def execute(log_group_name:)
+      cloudwatch_logs_gateway.healthy?(
+        log_group_name: log_group_name,
+        filter_pattern: HEALTHY_MATCH_PATTERN
+      )
+    end
+
+    private
+
+    HEALTHY_MATCH_PATTERN = 'Access-Accept'.freeze
+
+    attr_reader :cloudwatch_logs_gateway
   end
 end
--- spec/use_case/health_check_spec.rb
+++ spec/use_case/health_check_spec.rb
@@ -1,8 +1,20 @@
 describe UseCase::HealthCheck do
-  subject { described_class.new(gateway: cloudwatch_gateway) }
-  let(:cloudwatch_logs_gateway) { double(gt) }
+  subject do
+    described_class.new(
+      cloudwatch_logs_gateway: cloudwatch_logs_gateway
+    )
+  end
 
-  context 'it calls check_health on the gateway' do
+  let(:cloudwatch_logs_gateway) { double(healthy?: nil) }
 
+  context 'Given the Cloudwatch Logs Gateway' do
+    it 'calls healthy? on it' do
+      subject.execute(log_group_name: 'SomeLogGroupName')
+      expect(cloudwatch_logs_gateway).to have_received(:healthy?)
+        .with(
+          log_group_name: 'SomeLogGroupName',
+          filter_pattern: 'Access-Accept'
+        )
+    end
   end
 end
