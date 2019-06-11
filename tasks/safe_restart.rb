23d674f23602906fdf8b60fcf6e7ecd653cd5d07
Emile Swarts <emile.swarts123@gmail.com>
2019-06-11 09:52:41 +0100

    Fix bug with initializing the health check use case (#53)
    There is no need to use default arguments, explicitly initialize the HealthCheck
    use case

--- tasks/safe_restart.rb
+++ tasks/safe_restart.rb
@@ -17,14 +17,13 @@ task :safe_restart, :environment do |_, args|
     ecs_gateway = Gateway::Aws::Ecs.new(config: { region: region })
     route53_gateway = Gateway::Aws::Route53.new
     cluster_finder = UseCase::FindClustersForEnvironment.new(gateway: ecs_gateway, environment: environment)
-    health_checker = UseCase::HealthCheck.new(route53_gateway: route53_gateway)
-    delayer = Gateway::Delayer.new
+    health_checker = UseCase::HealthCheck.new(route53_gateway: route53_gateway, delayer: Gateway::Delayer.new)
 
     UseCase::SafeRestart.new(
       cluster_finder: cluster_finder,
       ecs_gateway: ecs_gateway,
       health_checker: health_checker,
-      delayer: delayer,
+      delayer: Gateway::Delayer.new,
       logger: logger
     ).execute
   end
